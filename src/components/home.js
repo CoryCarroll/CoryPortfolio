50205c2e96ebdbade0f3003b35d7dea308245ec5
Cory Carroll <corystory2@gmail.com>
2024-02-14 15:37:17 -0700

    adding tailwind

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ import NavBar from './navBar';
 
 const Home = () => {
     return (
-        <div className='home-container bg-gradient-to-r from-gray-700 via-green-700 to-gray-700 p-10' >
+        <div className='home-container bg-black' >
             <NavBar />
             <Header />
             <About />
